57bcb7d290aabe72a4d0600e05dfe1cc5954f118
mrrobot911 <kelwin2705@gmail.com>
2024-01-26 17:04:22 +0300

    feat: add read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
-const read = async () => {
-    // Write your code here 
+import fs from "node:fs";
+import { createPathName } from "../utils/pathName.js";
+import { fileURLToPath } from "node:url";
+import { isExists } from "../utils/isExist.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const sorcePath = await createPathName(__filename, "files", "fileToRead.txt");
+
+const read = async (sorcePath) => {
+  if (!(await isExists(sorcePath))) {
+    throw new Error("fs operation failed");
+  }
+  const stream = fs.createReadStream(sorcePath, { encoding: "utf8" });
+  stream.on("data", (chunk) => {
+    process.stdout.write(chunk);
+  });
 };
 
-await read();
+await read(sorcePath);
