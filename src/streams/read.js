81a5c478c4de1de86b771887447ec98a0efa90f2
mrrobot911 <kelwin2705@gmail.com>
2024-01-26 22:18:07 +0300

    feat: add stream/read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
-const read = async () => {
-    // Write your code here 
+import fs from "node:fs";
+import { createPathName } from "../utils/pathName.js";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const sorcePath = await createPathName(__filename, "files", "fileToRead.txt");
+
+const read = async (sorcePath) => {
+  const stream = fs.createReadStream(sorcePath, { encoding: "utf8" });
+  stream.on("data", (chunk) => {
+    process.stdout.write(chunk);
+  });
 };
 
-await read();
+await read(sorcePath);
