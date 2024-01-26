c816ec0eb76c092e1b68c830e0924cfb22a46531
mrrobot911 <kelwin2705@gmail.com>
2024-01-26 17:12:41 +0300

    feat: add cli/env function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+  const filteredVariables = Object.entries(process.env)
+    .filter(([key]) => key.startsWith("RSS_"))
+    .map(([key, value]) => `${key}=${value}`)
+    .join("; ");
+  console.log(filteredVariables);
 };
 
 parseEnv();
