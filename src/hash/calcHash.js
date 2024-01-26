74c2053efbb7db0a0140726d6a4a6c865ba0c459
mrrobot911 <kelwin2705@gmail.com>
2024-01-26 21:27:08 +0300

    feat: add hash/calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,26 @@
-const calculateHash = async () => {
-    // Write your code here 
+import fs from "node:fs";
+import crypto from "crypto";
+import { createPathName } from "../utils/pathName.js";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const sorcePath = await createPathName(
+  __filename,
+  "files",
+  "fileToCalculateHashFor.txt"
+);
+
+const calculateHash = async (sorcePath) => {
+  const hash = crypto.createHash("sha256");
+
+  const readStream = fs.createReadStream(sorcePath);
+  readStream.on("data", (chunk) => {
+    hash.update(chunk);
+  });
+  readStream.on("end", () => {
+    const fileHash = hash.digest("hex");
+    console.log(fileHash);
+  });
 };
 
-await calculateHash();
+await calculateHash(sorcePath);
