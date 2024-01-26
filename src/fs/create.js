4efa4e9f167c27e25ea156e9a8b2245c3d631dac
mrrobot911 <kelwin2705@gmail.com>
2024-01-26 15:26:55 +0300

    feat: add create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
-const create = async () => {
-    // Write your code here 
+import fs from "node:fs";
+import { fileURLToPath } from "node:url";
+import { isExists } from "../utils/isExist.js";
+import { createPathName } from "../utils/pathName.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const name = await createPathName(__filename, "files", "fresh.txt");
+
+const create = async (name) => {
+  if (await isExists(name)) {
+    throw new Error("fs operation failed");
+  }
+  const writeStream = fs.createWriteStream(name);
+  writeStream.end("I am fresh and young", "utf-8");
 };
 
-await create();
+await create(name);
